f23afb59704930d04807ad1ae0365a90ffb8cfcb
Ilya <ilyaguzin@MacBook-Pro-Ilya.local>
2026-04-03 16:23:19 +0300

    Add schemas/repositories/services

--- book-service/app/database.py
+++ book-service/app/database.py
@@ -4,8 +4,7 @@ from sqlalchemy.orm import sessionmaker
 from .config import settings
 
 engine = create_engine(
-    settings.database_url,
-    connect_args={"check_same_thread": False}  
+    settings.database_url
 )
 
 SessionLocal = sessionmaker(autocommit=False, autoFile=False, bind=engine)
